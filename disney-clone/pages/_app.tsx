a2896e2afd912a3a05aa0c449dc90cdb1076d022
Nii Adotei <61387678+niiadoteibrown@users.noreply.github.com>
2022-12-15 20:55:35 +0000

    clean up

---
+++ disney-clone/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../globalstyles'
+
+const theme: DefaultTheme = {
+  primary: '#111',
+  secondary: '#0070f3',
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
